ffb79141b6d89b34fe571c5a47e96c9c559ab512
Magnusson Oscar <es014965@ettegroup.local>
2018-06-29 16:44:07 +0200

    Prevent screen lock or screen saver when app is on

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-const {app, BrowserWindow, Menu, ipcMain} = require('electron')
+const {app, BrowserWindow, Menu, ipcMain, powerSaveBlocker} = require('electron')
 const url = require('url');
 const path = require('path');
-
 
 
 let mainWindow;
@@ -11,63 +10,72 @@ let speed;
 let cursor;
 let fullscreen;
 let wait;
+let powerSaveBlockerID;
 
 app.on('ready', createWindow);
-  function createWindow () {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600})
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600})
 
-    mainWindow.loadURL(url.format({
-      pathname: path.join(__dirname, './src/index.html'),
-      protocol: 'file',
-      slashes: true
-    }));
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, './src/index.html'),
+    protocol: 'file',
+    slashes: true
+  }));
 
-    //Quit app when closed
-    mainWindow.on('closed', function() {
-      app.quit();
-    });
+  //Quit app when closed
+  mainWindow.on('closed', function() {
+    powerSaveBlocker.stop(powerSaveBlockerID);
+    app.quit();
+  });
 
+  const powerSaveBlockerID = powerSaveBlocker.start('prevent-display-sleep');
+}
+
+function createPreviewWindow() {
+  // Create the browser window.
+  //KEEP FRAME: TRUE FOR THE MOMENT
+  previewWindow = new BrowserWindow({frame: !fullscreen, fullscreen: fullscreen, width: 600, height: 500, title: 'New Window'})
+
+  previewWindow.loadURL(url.format({
+    pathname: path.join(__dirname, './src/preview.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+
+  //Garbage collection handle
+  previewWindow.on('closed', function() {
+    previewWindow = null;
+  });
+
+  //Send over array of sentences
+  previewWindow.custom = {
+    'array': arrayOfSentences,
+    'speed': speed,
+    'cursor': cursor,
+    'wait': wait
   }
 
-  function createPreviewWindow() {
-    // Create the browser window.
-    //KEEP FRAME: TRUE FOR THE MOMENT
-    previewWindow = new BrowserWindow({frame: !fullscreen, fullscreen: fullscreen, width: 600, height: 500, title: 'New Window'})
 
-    previewWindow.loadURL(url.format({
-      pathname: path.join(__dirname, './src/preview.html'),
-      protocol: 'file',
-      slashes: true
-    }));
-
-      //Garbage collection handle
-      previewWindow.on('closed', function() {
-        previewWindow = null;
-      });
-
-      //Send over array of sentences
-      previewWindow.custom = {
-        'array': arrayOfSentences,
-        'speed': speed,
-        'cursor': cursor,
-        'wait': wait
-      }
-  }
-
-  //Recieve the array of sentences from index.js and sends to preview.js
-  ipcMain.on('createPreviewWindow', function(e, array){
-    speed = array[0];
-    arrayOfSentences = array[1];
-    cursor = array[2];
-    fullscreen = array[3];
-    wait = array[4];
-    createPreviewWindow();
-  })
+  //Prevents the application
 
 
-  //Create menu template
-  const mainMenuTemplate = [
+
+}
+
+//Recieve the array of sentences from index.js and sends to preview.js
+ipcMain.on('createPreviewWindow', function(e, array){
+  speed = array[0];
+  arrayOfSentences = array[1];
+  cursor = array[2];
+  fullscreen = array[3];
+  wait = array[4];
+  createPreviewWindow();
+})
+
+
+//Create menu template
+const mainMenuTemplate = [
   {
     label: 'File',
     submenu:[
@@ -83,28 +91,28 @@ app.on('ready', createWindow);
 
     ]
   }
-  ]
+]
 
-  //Add developer tools item if not in production
-  if(process.env.NODE_ENV !== 'production'){
-    mainMenuTemplate.push({
-      label: 'Dev Tools',
-      submenu:[
-        {
-          label: 'Open Dev Tools',
-          click(item, focusedWindow){
-            focusedWindow.toggleDevTools();
-          }
-        },{
-          role: 'reload'
+//Add developer tools item if not in production
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Dev Tools',
+    submenu:[
+      {
+        label: 'Open Dev Tools',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
         }
+      },{
+        role: 'reload'
+      }
 
-      ]
-    });
-  }
+    ]
+  });
+}
 
-  //Build menu from template
-  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+//Build menu from template
+const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 
-  //Insert Menu
-  Menu.setApplicationMenu(mainMenu);
+//Insert Menu
+Menu.setApplicationMenu(mainMenu);
